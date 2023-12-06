db69ab7fae85db10ae31bbc3e41c0664137752e7
vorchris <christian.vor26@gmail.com>
2023-12-06 08:39:35 +0100

    HÜ

--- SQL_Tutorial/src/com/Christian/Main.java
+++ SQL_Tutorial/src/com/Christian/Main.java
@@ -5,12 +5,14 @@ import java.util.Random;
 import java.util.StringJoiner;
 
 public class Main {
-    private static final String URL ="jdbc:sqlite:C:\\Users\\chris\\Desktop\\sqlite-tools-win32-x86-3430100\\sqlite-tools-win32-x86-3430100\\vorhofer.db";
+    private static final String URL = "jdbc:mysql://localhost/vorhofer?useTimezone=true&serverTimezone=UTC";
+    private static String username = "root";
+    private static String password = "root";
     private static Connection conn = null;
     private void connect() {
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection(URL);
+            conn = DriverManager.getConnection(URL, username, password);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
@@ -28,32 +30,44 @@ public class Main {
         return returnValue;
     }
 
-    public static ResultSet getValuesFromDb(String q){
-        ResultSet rs = null;
-        try (Statement stmt = conn.createStatement()){
-            rs = stmt.executeQuery(q);
+    public static String getValuesFromDb(String q) {
+        try (Connection conn = DriverManager.getConnection(URL, username, password);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(q)) {
+            if (rs.next()) {
+                return rs.getString(1);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return rs;
+        return null;
     }
+
     public void makeAndFill(Main m, String tableName){
         Random rnd = new Random();
         m.executeQuery("DROP TABLE IF EXISTS " + tableName);
-        m.executeQuery("CREATE TABLE " + tableName + " (id INTEGER PRIMARY KEY AUTOINCREMENT, value Integer, value2 Integer);");
+        m.executeQuery("CREATE TABLE " + tableName + " (id INTEGER PRIMARY KEY AUTO_INCREMENT, value Integer, value2 Integer);");
         for (int i = 0; i < 20; i++) {
             int rndInt = rnd.nextInt(1, 11);
             m.executeQuery("INSERT INTO " + tableName + " (value, value2) VALUES (" + rndInt + ", " + rndInt%2 + ");");
         }
     }
-    public static String getEven(String tablename) throws SQLException {
-        ResultSet even = getValuesFromDb("SELECT count(value) FROM " + tablename + " WHERE value%2 == 0;");
-        return even.getString(1);
+    public static String getEven(String tablename) {
+        String evenCount = getValuesFromDb("SELECT COUNT(value) FROM " + tablename + " WHERE value % 2 = 0;");
+        if (evenCount != null) {
+            return evenCount;
+        }
+        return "0";
     }
-    public static String getOdd(String tablename) throws SQLException {
-        ResultSet even = getValuesFromDb("SELECT count(value) FROM " + tablename + " WHERE value%2 == 1;");
-        return even.getString(1);
+    public static String getOdd(String tablename) {
+        String oddCount = getValuesFromDb("SELECT COUNT(value) FROM " + tablename + " WHERE value % 2 = 1;");
+        if (oddCount != null) {
+            return oddCount;
+        }
+        return "0";
     }
+
+
     public static void main(String[] args) throws SQLException{
         Main main = new Main();
         main.connect();
