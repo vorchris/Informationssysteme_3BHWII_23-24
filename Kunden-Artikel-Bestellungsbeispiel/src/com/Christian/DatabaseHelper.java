bac41c412614d033a3f11b62cff267b6e244bb0a
vorchris <christian.vor26@gmail.com>
2024-01-17 08:03:10 +0100

    Hausaufgaben added

--- Kunden-Artikel-Bestellungsbeispiel/src/com/Christian/DatabaseHelper.java
+++ Kunden-Artikel-Bestellungsbeispiel/src/com/Christian/DatabaseHelper.java
@@ -59,4 +59,110 @@ public class DatabaseHelper{
             e.printStackTrace();
         }
     }
+    static class Lager {
+        static{
+            DatabaseHelper.execute("CREATE TABLE IF NOT EXISTS lager (lagerId INT AUTO_INCREMENT, artikelId int NOT NULL, amount int NOT NULL, PRIMARY KEY (lagerId), FOREIGN KEY(artikelId) REFERENCES artikles(artikelId));");
+        }
+        public static void addLager(int artikelId, int amount){
+            DatabaseHelper.execute("INSERT INTO lager (artikelId, amount) VALUES (\"" + artikelId + "\", \"" + amount + "\");");
+        }
+        public static ResultSet showLager(){
+            return DatabaseHelper.executeQuery("SELECT * FROM lager;");
+        }
+        public static void restock(String Artikelname, int amount){
+            DatabaseHelper.execute("UPDATE lager SET amount = amount + " + amount + " WHERE artikelId = (SELECT artikelId FROM artikles WHERE name = \"" + Artikelname + "\");");
+        }
+        public static void updateAmount(int artikelId, int amount){
+            DatabaseHelper.execute("UPDATE lager SET amount = " + amount + " WHERE artikelId = " + artikelId + ";");
+        }
+        public static int getLagerbestand(int artikelId){
+            try (ResultSet rs = DatabaseHelper.executeQuery("SELECT amount FROM lager WHERE artikelId = " + artikelId + ";")){
+                if (rs.next()) {  // Move the cursor to the first row
+                    return rs.getInt("amount"); // Use column name or 1 instead of 0
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return 0;
+        }
+    }
+
+    static class Bestellungen {
+        static {
+            DatabaseHelper.execute("CREATE TABLE IF NOT EXISTS orders (orderId INT AUTO_INCREMENT, customerId int NOT NULL, artikelId int NOT NULL, amount int NOT NULL, PRIMARY KEY (orderId), FOREIGN KEY(customerId) REFERENCES customers(customerId), FOREIGN KEY(artikelId) REFERENCES artikles(artikelId));");
+        }
+        public static void addBestellung(int customerId, int artikelId, int amount){
+            if(DatabaseHelper.Lager.getLagerbestand(artikelId) >= amount) {
+                DatabaseHelper.execute("INSERT INTO orders (customerId, artikelId, amount) VALUES (\"" + customerId + "\", \"" + artikelId + "\", \"" + amount + "\");");
+                DatabaseHelper.Lager.updateAmount(artikelId, DatabaseHelper.Lager.getLagerbestand(artikelId) - amount);
+            }
+            else
+                System.out.println("Nicht genug Artikel auf Lager! (Lagerbestand: " + DatabaseHelper.Lager.getLagerbestand(artikelId) + ")");
+        }
+        public static ResultSet showBestellungen(){
+            return DatabaseHelper.executeQuery("SELECT \n" +
+                    "    o.orderId,\n" +
+                    "    c.name AS customerName,\n" +
+                    "    c.email,\n" +
+                    "    a.name AS artikelName,\n" +
+                    "    a.price,\n" +
+                    "    o.amount\n" +
+                    "FROM \n" +
+                    "    orders o\n" +
+                    "    JOIN customers c ON o.customerId = c.customerId\n" +
+                    "    JOIN artikles a ON o.artikelId = a.artikelId;\n");
+        }
+        public static void deleteBestellung(int id){
+            DatabaseHelper.execute("DELETE FROM orders WHERE orderId = " + id + ";");
+        }
+        public static void updateBestellung(int id, int customerId, int artikelId, int amount){
+            DatabaseHelper.execute("UPDATE orders SET customerId = \"" + customerId + "\", artikelId = \"" + artikelId + "\", amount = \"" + amount + "\" WHERE orderId = " + id + ";");
+        }
+    }
+    static class Artikel {
+        static {
+            DatabaseHelper.execute("CREATE TABLE IF NOT EXISTS arikles (id INT AUTO_INCREMENT, name CHAR(60), price decimal(10,2), PRIMARY KEY (id));");
+        }
+
+        public static void addArtikel(String name, double price, int amount) {
+            DatabaseHelper.execute("INSERT INTO artikles (name, price) VALUES (\"" + name + "\", \"" + (double) (Math.round(price * 100) / 100) + "\");");
+            try {
+                DatabaseHelper.Lager.addLager(DatabaseHelper.executeQuery("SELECT artikelId FROM artikles WHERE name = \"" + name + "\";").getInt(0), amount);
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+
+        public static ResultSet showArtikles() {
+            return DatabaseHelper.executeQuery("SELECT * FROM artikles;");
+        }
+
+        public static void deleteArtikel(int id) {
+            DatabaseHelper.execute("DELETE FROM artikles WHERE artikelId = " + id + ";");
+        }
+
+        public static void updateArtikel(int id, String name, double price) {
+            DatabaseHelper.execute("UPDATE artikles SET name = \"" + name + "\", price = \"" + (double) (Math.round(price * 100) / 100) + "\" WHERE artikelId = " + id + ";");
+        }
+    }
+
+    static class Kunden {
+        static{
+            DatabaseHelper.execute("CREATE TABLE IF NOT EXISTS customers (id INT AUTO_INCREMENT, name CHAR(60), email CHAR(60), PRIMARY KEY (id));");
+        }
+        public static void addKunde(String name, String email){
+            DatabaseHelper.execute("INSERT INTO customers (name, email) VALUES (\"" + name + "\", \"" + email + "\");");
+        }
+        public static ResultSet showKunden(){
+            return DatabaseHelper.executeQuery("SELECT * FROM customers;");
+        }
+        public static void deleteKunde(int id){
+            DatabaseHelper.execute("DELETE FROM customers WHERE customerId = " + id + ";");
+        }
+        public static void updateKunde(int id, String name, String email){
+            DatabaseHelper.execute("UPDATE customers SET name = \"" + name + "\", email = \"" + email + "\" WHERE customerId = " + id + ";");
+        }
+    }
+
+
 }
--- Kunden-Artikel-Bestellungsbeispiel/src/com/Christian/Main.java
+++ Kunden-Artikel-Bestellungsbeispiel/src/com/Christian/Main.java
@@ -1,46 +1,91 @@
 package com.Christian;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        new DatabaseHelper();
-        Artikel artikel = new Artikel();
-        Kunden kunden = new Kunden();
-        Bestellungen bestellungen = new Bestellungen();
         Scanner scr = new Scanner(System.in);
-        loop:
-        while (true){
-            System.out.println("####################################");
-            System.out.println("add Kunde (ak Name Email), show Kunden (sk); add Artikel (aa Name Preis(1.11)), show Artikel (sa); add Bestellung (ab) (CustomerID, ArtikelID, Anzahl), show Bestellungen (sb); exit (e)");
-            System.out.println("####################################");
+
+        while (true) {
+            displayMenu();
             String eingabe = scr.nextLine();
             String[] splitted = eingabe.split(" ");
-            switch (splitted[0]){
-                case "ak":
-                    kunden.addKunde(splitted[1], splitted[2]);
-                    break;
-                case "sk":
-                    DatabaseHelper.printResultSet(kunden.showKunden());
-                    break;
-                case "aa":
-                    artikel.addArtikel(splitted[1], Double.parseDouble(splitted[2]));
-                    break;
-                case "sa":
-                    DatabaseHelper.printResultSet(artikel.showArtikles());
-                    break;
-                case "ab":
-                    bestellungen.addBestellung(Integer.parseInt(splitted[1]), Integer.parseInt(splitted[2]), Integer.parseInt(splitted[3]));
-                    break;
-                case "sb":
-                    DatabaseHelper.printResultSet(bestellungen.showBestellungen());
-                    break;
-                case "e":
-                    break loop;
+
+            try {
+                switch (splitted[0]) {
+                    case "ak": // Add Kunde
+                        DatabaseHelper.Kunden.addKunde(splitted[1], splitted[2]);
+                        break;
+                    case "uk": // Update Kunde
+                        DatabaseHelper.Kunden.updateKunde(Integer.parseInt(splitted[1]), splitted[2], splitted[3]);
+                        break;
+                    case "sk": // Show Kunden
+                        printResultSet(DatabaseHelper.Kunden.showKunden());
+                        break;
+                    case "dk": // Delete Kunde
+                        DatabaseHelper.Kunden.deleteKunde(Integer.parseInt(splitted[1]));
+                        break;
+                    case "ua": // Update Artikel
+                        DatabaseHelper.Artikel.updateArtikel(Integer.parseInt(splitted[1]), splitted[2], Double.parseDouble(splitted[3]));
+                        break;
+                    case "aa": // Add Artikel
+                        DatabaseHelper.Artikel.addArtikel(splitted[1], Double.parseDouble(splitted[2]), Integer.parseInt(splitted[3]));
+                        break;
+                    case "sa": // Show Artikel
+                        printResultSet(DatabaseHelper.Artikel.showArtikles());
+                        break;
+                    case "da": // Delete Artikel
+                        DatabaseHelper.Artikel.deleteArtikel(Integer.parseInt(splitted[1]));
+                        break;
+                    case "ab": // Add Bestellung
+                        DatabaseHelper.Bestellungen.addBestellung(Integer.parseInt(splitted[1]), Integer.parseInt(splitted[2]), Integer.parseInt(splitted[3]));
+                        break;
+                    case "sb": // Show Bestellungen
+                        printResultSet(DatabaseHelper.Bestellungen.showBestellungen());
+                        break;
+                    case "al": // Add Lager
+                        DatabaseHelper.Lager.addLager(Integer.parseInt(splitted[1]), Integer.parseInt(splitted[2]));
+                        break;
+                    case "e": // Exit
+                        System.out.println("Exiting program.");
+                        return;
+                    default:
+                        System.out.println("Invalid command. Please try again.");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Error occurred: " + e.getMessage());
             }
         }
-        DatabaseHelper.close();
+    }
+
+    private static void displayMenu() {
+        System.out.println("####################################");
+        System.out.println("Commands:");
+        System.out.println("Add Customer (ak Name Email)");
+        System.out.println("Show Customers (sk)");
+        System.out.println("Update Customer (uk id Name Email)");
+        System.out.println("Delete Customer (dk id)");
+        System.out.println("Update Artikel (ua id Name Preis)");
+        System.out.println("Add Artikel (aa Name Preis Menge)");
+        System.out.println("Show Artikel (sa)");
+        System.out.println("Delete Artikel (da id)");
+        System.out.println("Add Bestellung (ab CustomerID ArtikelID Anzahl)");
+        System.out.println("Show Bestellungen (sb)");
+        System.out.println("Add to Lager (al ArtikelID Menge)");
+        System.out.println("Exit (e)");
+        System.out.println("####################################");
+    }
+
+    private static void printResultSet(ResultSet rs) throws SQLException {
+        while (rs != null && rs.next()) {
+            for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                System.out.print(rs.getMetaData().getColumnName(i) + ": " + rs.getString(i) + " ");
+            }
+            System.out.println();
+        }
     }
 }
